42da58f3cf38dbf359f13ce1de46756f967ae62b
shaileshj121 <shaileshbisht9876@gmail.com>
2024-03-15 08:51:57 +0530

    changes

--- noteUi/src/App.js
+++ noteUi/src/App.js
@@ -13,25 +13,25 @@ function App() {
   const [showDiv, setShowDiv] = useState(true);
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
+  const [notes, setNotes] = useState([
+    // all the notes will be here 
+  ])
+  const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState("")
 
 
   const toggleDiv = () => {
     setShowDiv(!showDiv);
   };
 
-  const [notes, setNotes] = useState([
-    // all the notes will be here 
-  ])
-
-
   const fatchnotes = async () => {
     await axios.get("http://localhost:4000/api/getNotes").then((response) => {
       setNotes(response.data)
-
     }).catch((err) => console.log("server error"))
   }
 
-  useEffect(() => { fatchnotes() })
+  useEffect(() => { fatchnotes() }, [])
+
 
   const addnotes = async (event) => {
     event.preventDefault();
@@ -42,7 +42,7 @@ function App() {
     }).then().catch((err) => {
       console.log(`error on send notes: ${err}`)
     })
-
+    fatchnotes()
     setTitle("")
     setContent("")
 
@@ -93,28 +93,28 @@ function App() {
 
     const newnoteId = noteId
 
+    setLoading(true)
+    fatchnotes()
+    
     await axios.delete("http://localhost:4000/api/notes", {
       data: {
         "noteNumber": newnoteId
       }
-    }).then()
+    }).then(setLoading(false),fatchnotes())
       .catch((err) => {
         console.log(`error on deleting note: ${err}`)
-      })
-    // const updateNotes = notes.filter((note) => note.id !== noteId);
-    // setNotes(updateNotes);
-    // setsubmit(true)
-
+      });
+      
   }
 
   return (
 
-    <div className="notes flex justify-start h-screen bg-gray-100 relative" >
+    <div className="notes flex bg-gray-800 justify-start h-screen bg-gray-100 relative" >
       <div className=" h-6 w-6 cursor-pointer block relative top-0">
         <svg xmlns="<http://www.w3.org/2000/svg>" id="notetoggle" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={toggleDiv}><path d="M4 6h16M4 12h16M4 18h16" /></svg>
       </div>
 
-      {showDiv ? <div className="w-1/7 bg-white shadow-lg rounded-lg p-8 relative">
+      {showDiv ? <div className="w-1/7 bg-gray-400 shadow-lg rounded-lg p-8 mr-2 relative">
 
         <form onSubmit={(event) => (selectedNote ? handelUpdateNote(event) : addnotes(event))}>
           <div className="mb-4">
@@ -147,21 +147,23 @@ function App() {
           </div>
         </form>
       </div> : null}
-      <div className="w-full bg-white shadow-lg rounded-lg  conent-note">
-        <div className="flex flex-wrap justify-center sm:justify-start relative">
-
-          {notes.map((note) => (
-            <div key={note.noteId} className="flex-initial w-64 sm:w-1/2 md:w-1/3 lg:w-1/4 relative ">
+      <div className="w-full bg-gray-800 shadow-lg rounded-lg  conent-note">
+        <div className="bg-gray-700	 p-2">
+          <input name= "search" value = {search} onChange={(event) => {setSearch(event.target.value)}} type="text" placeholder="Search.."/>
+        </div>
+        <div className="flex flex-wrap justify-center sm:justify-start relative overflow-hidden">
+        {loading? <div>please wait data is deleating... </div> : notes.map((note) => (
+            <div key={note.noteId} className="flex-initial hover:scale-105 ease-in-out duration-300 w-64 sm:w-1/2 md:w-1/3 lg:w-1/4 relative ">
               <div onClick={() => handelClickNote(note)}>
                 <button onClick={(event) => deletenote(event, note.noteId, note.title)} className="absolute top-3 right-5 font-bold">X</button>
-                <div className=" border-solid border-2 rounded-md border-sky-500 p-5 m-3">
-                  <h2 className="font-bold pb-2">{note.title}</h2>
-                  <p>{note.content}</p>
+                <div className=" bg-gray-600  border-solid rounded-md m-3">
+                  <h2 className="font-bold text-slate-50 pl-5 pt-2 pb-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">{note.title}</h2>
+                  <p className = "text-slate-50  p-5 pt-2">{note.content}</p>
                 </div>
               </div>
             </div>
-          )
-          )}
+          ))
+        }
 
         </div>
 
--- noteUi/tailwind.config.js
+++ noteUi/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '60': '60vh',
+      }
+    },
   },
   plugins: [],
 }
