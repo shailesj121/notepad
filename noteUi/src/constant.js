9a68b3e17556bb4df081f98ebbc058119c442c56
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-04 15:42:21 +0530

    changes in both front and backend

--- noteUi/src/constant.js
+++ noteUi/src/constant.js
@@ -1,1 +1,1 @@
-export const FATCH_URL_API = "http://localhost:4000/api" 
+export const FATCH_URL_API = "http://localhost:4000/api" || "https://notepad-m5xg-o62wundwx-shailesh-bishts-projects.vercel.app/api"
