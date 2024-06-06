723ad574e7eab7e653d9d4669b452d502ee5e1fd
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-06 11:02:52 +0530

    changes for local default

---
+++ noteUi/src/services/signup.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import {Auth_URL_API} from "../constant"
+
+export const signUp = async(url, data)=> {
+    try {
+        return await axios.post(`${Auth_URL_API}${url}`, data) 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
