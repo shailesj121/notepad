47289ab16b0a694337908fd1878bf9e91d9870cf
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-11 15:33:52 +0530

    asf

--- noteUi/src/constant.js
+++ noteUi/src/constant.js
@@ -1,4 +1,4 @@
 export const FATCH_URL_API = "http://localhost:4000/api" 
 export const Auth_URL_API = "http://localhost:4000/auth" 
-export const Varcel_Fatch_Url_API =  "http://notepadbackend-shailesh-bishts-projects.vercel.app/api"
-export const Varcel_Auth_Url =  "http://notepadbackend-shailesh-bishts-projects.vercel.app/auth"
+export const Varcel_Fatch_Url_API =  "https://notepadbackend-shailesh-bishts-projects.vercel.app/api"
+export const Varcel_Auth_Url =  "https://notepadbackend-shailesh-bishts-projects.vercel.app/auth"
