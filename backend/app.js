9a455c26ee37782cc4908d5d461e1a54d3fbf39e
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-11 14:36:24 +0530

    ,,

--- backend/app.js
+++ backend/app.js
@@ -24,3 +24,10 @@ import bodyParser from "body-parser";
 app.use(bodyParser.json())
 app.use("/api", router);
 app.use("/auth", userRouter)
+app.use("/", (req,res, next)=> {
+    res.status(200).json(
+        {
+            Message: "OK"
+        }
+    )
+})
