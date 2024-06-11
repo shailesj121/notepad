20879feb42600823e74ecf08955dfab1f0e2a8a0
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-11 14:25:14 +0530

    add to varcel

--- noteUi/src/services/signup.js
+++ noteUi/src/services/signup.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { Auth_URL_API } from "../constant"
+import { Varcel_Auth_Url } from "../constant"
 
 export const signUp = async (url, data) => {
     try {
-        return await axios.post(`${Auth_URL_API}${url}`, data, {
+        return await axios.post(`${Varcel_Auth_Url}${url}`, data, {
             withCredentials: true
         })
     } catch (error) {
@@ -13,7 +13,7 @@ export const signUp = async (url, data) => {
 
 export const login = async (url, data) => {
     try {
-        return await axios.post(`${Auth_URL_API}${url}`, data, {
+        return await axios.post(`${Varcel_Auth_Url}${url}`, data, {
             withCredentials: true
         })
     } catch (error) {
