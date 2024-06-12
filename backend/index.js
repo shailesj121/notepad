96b194aa509abb1fa13736516f428c446dfe43d3
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-12 09:50:48 +0530

    sa

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,8 @@
-import dotenv from "dotenv"
+import 'dotenv/config'
 import app from "./app.js"
 
 import database from "./db/database.js"
 
-dotenv.config({
-    path: './env'
-})
 
 database().then(() => {
     app.listen(process.env.PORT || 4000, () => { console.log(`server is listening at http://localhost:${process.env.PORT}/api/notes`) })
