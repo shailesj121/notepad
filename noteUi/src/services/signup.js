13e545b5e7ab7c024fa4070a44aaf88e101b734b
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-12 10:11:08 +0530

    change url for signup and signin

--- noteUi/src/services/signup.js
+++ noteUi/src/services/signup.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { Varcel_Fatch_Url_API } from "../constant"
+import { Varcel_Auth_Url } from "../constant"
 
 export const signUp = async (url, data) => {
     try {
-        return await axios.post(`${Varcel_Fatch_Url_API}${url}`, data, {
+        return await axios.post(`${Varcel_Auth_Url}${url}`, data, {
             withCredentials: true
         })
     } catch (error) {
@@ -13,7 +13,7 @@ export const signUp = async (url, data) => {
 
 export const login = async (url, data) => {
     try {
-        return await axios.post(`${Varcel_Fatch_Url_API}${url}`, data, {
+        return await axios.post(`${Varcel_Auth_Url}${url}`, data, {
             withCredentials: true
         })
     } catch (error) {
