1734edab439926e234e994aaf87062eba146861c
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-12 11:03:26 +0530

    a

--- noteUi/src/index.jsx
+++ noteUi/src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
-// import NoPage from "./pages/noPage";
+import NoPage from "./pages/noPage";
 import Home from "./pages/Notes";
 import Signup from "./pages/SignUp";
 import  {ProtactedRoute}  from "./utils/protactedRoute";
@@ -13,12 +13,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-        {/* <Route path="/" element={<NoPage />} /> */}
-        <Route index path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+      <Routes>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
         <Route element={<ProtactedRoute />}>
-          <Route path="/user" element={<Home />} />
+          <Route index path="/" element={<Home />}/>
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
