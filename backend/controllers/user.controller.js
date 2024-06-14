e51573bc370582ae47e1540252dc0cd7a06ba3c3
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-14 17:08:10 +0530

    dsds

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -58,7 +58,7 @@ const loginUser = asyncHandler(async (req, res) => {
     console.log(refreshToken)
 
     res.cookie("Refresh_token", refreshToken, {
-        domain: ".notepad-rho-pink.vercel.app",
+        domain: "notepad-rho-pink.vercel.app",
         secure: true,
     }).status(200).json({
         message: "user Successfully login"
