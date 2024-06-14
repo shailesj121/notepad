468d9696d9ee556174ccdfc95e28d1c2fcda6382
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-14 15:46:52 +0530

    fvs

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -58,6 +58,7 @@ const loginUser = asyncHandler(async (req, res) => {
     console.log(refreshToken)
 
     res.cookie("Refresh_token", refreshToken, {
+        secure: false,
         sameSite: "none"
 
     }).status(200).json({
