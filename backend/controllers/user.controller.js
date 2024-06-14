5c14b087cb8f970b255b30d773b21b2dec2a4fea
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-14 16:58:20 +0530

    f

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -58,7 +58,7 @@ const loginUser = asyncHandler(async (req, res) => {
     console.log(refreshToken)
 
     res.cookie("Refresh_token", refreshToken, {
-        path: "/login",
+        domain: "notepadbackend-shailesh-bishts-projects.vercel.app",
         secure: true,
         sameSite: "Strict"
     }).status(200).json({
