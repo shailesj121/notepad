c65be0f2d5ae26002cb040faed34adca8fe1864e
shaileshj121 <shaileshbisht9876@gmail.com>
2024-06-14 18:36:46 +0530

    sv

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,15 @@ import router from "./routes/notes.route.js";
 import userRouter from "./routes/user.route.js"
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const apiProxy = createProxyMiddleware('/', {
+    target: 'https://notepadbackend-shailesh-bishts-projects.vercel.app',
+    changeOrigin: true, 
+  });
 
 app.use(bodyParser.json())
 app.use(cookieParser());
+app.use('/', apiProxy);
 app.use("/api", router);
 app.use("/auth", userRouter)
