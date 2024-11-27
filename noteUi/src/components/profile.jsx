2e1a012d923b438576b903ab0a707c946fc10a01
shailesj121 <shaileshbisht9876@gmail.com>
2024-11-27 10:30:13 +0530

    add fev icon

---
+++ noteUi/src/components/profile.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Profile = ()=>{
+
+    return (
+    <>
+    <div>
+        <h1>Your Profile</h1>
+        <img src="/" alt="user"></img>
+        <h4>Name</h4>
+        <span>Total Notes: 0</span>
+    </div>
+    </>
+    )
+}
+
+export default Profile
